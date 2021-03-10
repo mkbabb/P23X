a452e3b2b5989e267de0d773324512e509b03e04
Mike Babb <mike7400@gmail.com>
2021-03-10 15:47:50 -0500

    torbs

--- TABS/tabs.asm
+++ TABS/tabs.asm
@@ -6,7 +6,7 @@
 print_space:
     mov dl, ' '
 print_space_loop:
-    int 21h                     ; int'rupt.
+    int 21h                     ; int'rupt 2 print.
     loopz print_space_loop
 main_body:
     mov cl, byte ptr es:[82h]
@@ -16,25 +16,25 @@ main_body:
     mov cl, 10
 main_loop:
     mov ah, 08h                 ; read from stdin into al, no echo.
-    int 21h                     ; int'rupt.
+    int 21h                     ; int'rupt thereof.
     mov dl, al
 
-    mov ah, 02h                 ; print to stdout from dl.
+    mov ah, 02h                 ; code 2 print; will be int'rupt'd later.
 
     cmp al, 09h
-    je print_space
+    je print_space              
 
-    int 21h                     ; int'rupt.
+    int 21h                     ; int'rupt 2 print.
 
-    cmp al, 0dh
+    cmp al, 0dh                 ; a \n? jmp to reset tab counter.
+    je main_body                
+    cmp al, 0ah                 ; a \r? jmp to reset tab counter.
     je main_body
-    cmp al, 0ah
-    je main_body
-    cmp al, 1ah
+    cmp al, 1ah                 ; a EOF? jmp to done.
     je done
 
     loopnz main_loop
-    jmp main_body
+    jmp main_body               
 done:
     mov ah, 4ch                 
     int 21h  
