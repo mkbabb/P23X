93b6abcd2b0d2c2756d68746880357f789f46703
Mike Babb <mike7400@gmail.com>
2021-03-10 14:53:18 -0500

    finally

--- TABS/tabs_old.asm
+++ TABS/tabs_old.asm
@@ -3,27 +3,29 @@
         .stack     256 
 .code
    
-print_space:
-    mov dl, ' '
-print_space_loop:
-    int 21h                     ; int'rupt.
-    loopz print_space_loop
+default:
+    mov bl, 10
+    jmp main_body
+
+get_tab_count:
+    cmp byte ptr es:[80h],0
+    je default
+
+    mov bl, byte ptr es:[82h]
+    sub bl, '0'
+   
 main_body:
-    mov cl, byte ptr es:[82h]
-    sub cl, '0'
-    jnc main_loop
-    
-    mov cl, 10
+    mov cl, bl
+
 main_loop:
     mov ah, 08h                 ; read from stdin into al, no echo.
     int 21h                     ; int'rupt.
     mov dl, al
 
-    mov ah, 02h                 ; print to stdout from dl.
-
     cmp al, 09h
     je print_space
 
+    mov ah, 02h                 ; print to stdout from dl.
     int 21h                     ; int'rupt.
 
     cmp al, 0dh
@@ -33,10 +35,21 @@ main_loop:
     cmp al, 1ah
     je done
 
-    loopnz main_loop
+    dec cx
+    jcxz main_body
+    jmp main_loop
+
+print_space:
+    mov dl, ' '
+print_space_loop:
+    mov ah, 02h                 ; print to stdout from dl.
+    int 21h                     ; int'rupt.
+    loop print_space_loop
+
     jmp main_body
+
 done:
     mov ah, 4ch                 
     int 21h  
 
-    end main_body  
+    end get_tab_count  
