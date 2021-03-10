f18e37c4ca3237810f69be98b4294d8d092c4978
Mike Babb <mike7400@gmail.com>
2021-03-10 01:09:52 -0500

    tmps2

--- TABS/tabs.asm
+++ TABS/tabs.asm
@@ -3,18 +3,21 @@
         .stack     256 
 .code
    
-
+default:
+    mov bl, 10
+    jmp main_body
 
 get_tab_count:
-    mov bl, 10 
     cmp byte ptr es:[80h],0
-    je main_body
+    je default
 
     mov bl, byte ptr es:[82h]
     sub bl, '0'
+   
 main_body:
     xor cx, cx
     mov cl, bl
+
 main_loop:
     mov ah, 08h                 ; read from stdin into al, no echo.
     int 21h                     ; int'rupt.
@@ -26,23 +29,23 @@ main_loop:
     mov ah, 02h                 ; print to stdout from dl.
     int 21h                     ; int'rupt.
 
-    cmp al, 1ah
-    je done
-
     cmp al, 0dh
     je main_body
     cmp al, 0ah
     je main_body
+    cmp al, 1ah
+    je done
 
+    dec cx
     jcxz main_body
-    loop main_loop
+    jmp main_loop
 
 print_space:
     mov dl, ' '
 print_space_loop:
     mov ah, 02h                 ; print to stdout from dl.
     int 21h                     ; int'rupt.
-    loop print_space
+    loop print_space_loop
 
     jmp main_body
 
