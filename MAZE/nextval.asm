07f92d3f8297f648d4265f5b5e1d2d8ffd84903a
Mike Babb <mike7400@gmail.com>
2021-03-21 21:15:02 -0400

    tomp

--- MAZE/nextval.asm
+++ MAZE/nextval.asm
@@ -17,40 +17,79 @@
 ; Date:      Update Reason
 ; --------------------------
 ; 11/06/2016 Original version
-;
-;
 ;---------------------------------------
          .model    small               ;64k code and 64k data
          .8086                         ;only allow 8086 instructions
          public    nextval             ;allow extrnal programs to call
 ;---------------------------------------
+         .data                        
+         .code                       
+nextval:
+    push ax
+    push cx
+    push bp
+calc_offset:
+    xor ax, ax
+    xor cx, cx
 
+    mov al, BYTE PTR [di]
+    sub al, 1
+    
+    mov cl, 30
 
-;---------------------------------------
-         .data                         ;start the data segment
-;---------------------------------------
+    mul cl
 
+    add al, BYTE PTR [si]
+    sub al, 1
 
-;---------------------------------------
-         .code                         ;start the code segment
-;---------------------------------------
-; Save any modified registers
-;---------------------------------------
-nextval:                               ;
-                                       ;
-                                       ;
-;---------------------------------------
-; Code to make 1 move in the maze
-;---------------------------------------
-                                       ;
-                                       ;
-                                       ;
-;---------------------------------------
-; Restore registers and return
-;---------------------------------------
-exit:                                  ;
-                                       ;
-         ret                           ;return
-;---------------------------------------
-         end
-
+    add bp, ax
+nextval_main:
+    cmp BYTE PTR [bx], 1
+    je testn
+    cmp BYTE PTR [bx], 2
+    je teste
+    cmp BYTE PTR [bx], 3
+    je tests
+    cmp BYTE PTR [bx], 4
+    je testw
+testn:
+    cmp BYTE ptr [bp - 30], ' '
+    jne teste
+
+    sub WORD PTR [di], 1
+
+    mov BYTE ptr [bx], 4
+    jmp next_val_exit
+teste:
+    cmp BYTE ptr [bp + 1], ' '
+    jne tests
+
+    add WORD PTR [si], 1
+
+    mov BYTE ptr [bx], 1
+    jmp next_val_exit
+tests:
+    cmp BYTE ptr [bp + 30], ' '
+    jne testw
+
+    add WORD PTR [di], 1
+
+    mov BYTE ptr [bx], 2
+    jmp next_val_exit
+testw:
+    cmp BYTE ptr [bp - 1], ' '
+    jne testn
+
+    sub WORD PTR [si], 1
+
+    mov BYTE ptr [bx], 3
+    jmp next_val_exit
+
+next_val_exit:
+    pop bp
+    pop cx
+    pop ax
+    ret
+
+exit:
+    end   
